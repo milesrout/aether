679a6765ba8a1c7ac4b7c8c5cced850c92c886a2
Miles Rout <miles@rout.nz>
2022-03-07 14:10:45 +1300

    cleanup: Add missing license comments

--- include/persist.h
+++ include/persist.h
@@ -1,3 +1,19 @@
+/* This file is part of Æther.
+ *
+ * Æther is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Æther is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 extern int persist_read(uint8_t **pbuf, size_t *psize, const char *filename, const char *password, size_t password_size);
 extern int persist_write(const char *filename, const uint8_t *buf, size_t size, const char *password, size_t password_size);
 extern int persist_loadbytes(uint8_t *buf, size_t size, const uint8_t **pbuf, size_t *psize);
